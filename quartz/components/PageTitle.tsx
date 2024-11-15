28224c55912eb5165ec2e7bf098170ce9c6329fa
tiennt <joshir634@gmail.com>
2024-11-15 15:30:29 +0700

    Update PageTitle.tsx

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -5,7 +5,7 @@ import { i18n } from "../i18n"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   //const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
-  const title = "🪴 HT Naruto"
+  const title = "Project VHT"
 
   const baseDir = pathToRoot(fileData.slug!)
   return (
